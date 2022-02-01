4d714adafb4f07916782531f026f5f2768c7d0b5
Peter Ebell <pb.ebell@gmail.com>
2022-02-01 13:51:31 +0100

    Payments API

--- src/Api/Payment.ts
+++ src/Api/Payment.ts
@@ -1,10 +1,10 @@
-import ApiAdapter from "../ApiAdapter";
-import Session from "../Session";
+import ApiAdapter from '../ApiAdapter';
+import Session from '../Session';
 
-import ApiEndpointInterface from "../Interfaces/ApiEndpointInterface";
-import Amount from "../Types/Amount";
-import CounterpartyAlias from "../Types/CounterpartyAlias";
-import PaginationOptions from "../Types/PaginationOptions";
+import ApiEndpointInterface from '../Interfaces/ApiEndpointInterface';
+import Amount from '../Types/Amount';
+import CounterpartyAlias from '../Types/CounterpartyAlias';
+import PaginationOptions from '../Types/PaginationOptions';
 
 export default class Payment implements ApiEndpointInterface {
     ApiAdapter: ApiAdapter;
@@ -26,15 +26,10 @@ export default class Payment implements ApiEndpointInterface {
      * @returns {Promise<void>}
      */
     public async get(userId: number, monetaryAccountId: number, paymentId: number, options: any = {}) {
-        const limiter = this.ApiAdapter.RequestLimitFactory.create("/payment");
+        const limiter = this.ApiAdapter.RequestLimitFactory.create('/payment');
 
-        const response = await limiter.run(async axiosClient =>
-            this.ApiAdapter.get(
-                `/v1/user/${userId}/monetary-account/${monetaryAccountId}/payment/${paymentId}`,
-                {},
-                {},
-                axiosClient
-            )
+        const response = await limiter.run(async (axiosClient) =>
+            this.ApiAdapter.get(`/v1/user/${userId}/monetary-account/${monetaryAccountId}/payment/${paymentId}`, {}, {}, axiosClient)
         );
 
         return response.Response[0];
@@ -47,14 +42,14 @@ export default class Payment implements ApiEndpointInterface {
      * @returns {Promise<void>}
      */
     public async list(
-        userId: number,
         monetaryAccountId: number,
         options: PaginationOptions = {
             count: 200,
             newer_id: false,
-            older_id: false
+            older_id: false,
         }
     ) {
+        const userId = this.Session.getUserId();
         const params: any = {};
 
         if (options.count !== undefined) {
@@ -67,16 +62,16 @@ export default class Payment implements ApiEndpointInterface {
             params.older_id = options.older_id;
         }
 
-        const limiter = this.ApiAdapter.RequestLimitFactory.create("/payment", "LIST");
+        const limiter = this.ApiAdapter.RequestLimitFactory.create('/payment', 'LIST');
 
-        const response = await limiter.run(async axiosClient =>
+        const response = await limiter.run(async (axiosClient) =>
             this.ApiAdapter.get(
                 `/v1/user/${userId}/monetary-account/${monetaryAccountId}/payment`,
                 {},
                 {
                     axiosOptions: {
-                        params: params
-                    }
+                        params: params,
+                    },
                 },
                 axiosClient
             )
@@ -94,23 +89,16 @@ export default class Payment implements ApiEndpointInterface {
      * @param options
      * @returns {Promise<void>}
      */
-    public async post(
-        userId: number,
-        monetaryAccountId: number,
-        description: string,
-        amount: Amount,
-        counterpartyAlias: CounterpartyAlias,
-        options: any = {}
-    ) {
-        const limiter = this.ApiAdapter.RequestLimitFactory.create("/payment", "POST");
+    public async post(userId: number, monetaryAccountId: number, description: string, amount: Amount, counterpartyAlias: CounterpartyAlias, options: any = {}) {
+        const limiter = this.ApiAdapter.RequestLimitFactory.create('/payment', 'POST');
 
-        const response = await limiter.run(async axiosClient =>
+        const response = await limiter.run(async (axiosClient) =>
             this.ApiAdapter.post(
                 `/v1/user/${userId}/monetary-account/${monetaryAccountId}/payment`,
                 {
                     counterparty_alias: counterpartyAlias,
                     description: description,
-                    amount: amount
+                    amount: amount,
                 },
                 {},
                 {},
